b48addf6862cd7d0c699e569406b9b7b8d6a886f
Ali Hassan <46684631+AliHassan7878@users.noreply.github.com>
2022-09-28 21:00:39 +0900

    WiFi camera streaming using python and opencv

---
+++ wifi_cam_streaming.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+
+cap = cv2.VideoCapture('rtsp://USERNAME:PASSWORD@CAM_IP/live')
+
+if not cap.isOpened():
+    print('Cannot open RTSP stream, check camera again')
+    exit(-1)
+
+while True:
+    _, frame = cap.read()
+    cv2.imshow('RTSP stream', frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
